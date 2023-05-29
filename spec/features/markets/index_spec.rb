8d4f63ffa5df1d93fc7c59c57b88363f8148bc90
Branden Ge <86636108+brandenge@users.noreply.github.com>
2023-05-29 11:36:35 -0500

    Fix: add it blocks to test code

--- spec/features/markets/index_spec.rb
+++ spec/features/markets/index_spec.rb
@@ -5,24 +5,36 @@ RSpec.feature 'Markets Index Page' do
     visit markets_path
   end
 
-  it 'has a header' do
+  it 'has headers' do
     expect(page).to have_content('Markets')
+
+    within '#index' do
+      within('#headers') do
+        expect(page).to have_content('Name')
+        expect(page).to have_content('City')
+        expect(page).to have_content('State')
+        expect(page).to have_content('More Info')
+      end
+    end
   end
 
-  within '#index' do
-    expect(page).to have_css('.market', count: 20)
-    within(first('.market')) do
-      within('.name') do
-        expect(page).to have_content("14&U Farmers' Market")
-      end
-      within('.city') do
-        expect(page).to have_content('Washington')
-      end
-      within('.state') do
-        expect(page).to have_content('District of Columbia')
-      end
-      within('.more_info') do
-        expect(page).to have_button('More Info')
+  it 'shows all the markets' do
+    within '#index' do
+      expect(page).to have_css('.market', count: 20)
+
+      within(first('.market')) do
+        within('.name') do
+          expect(page).to have_content("14&U Farmers' Market")
+        end
+        within('.city') do
+          expect(page).to have_content('Washington')
+        end
+        within('.state') do
+          expect(page).to have_content('District of Columbia')
+        end
+        within('.more_info') do
+          expect(page).to have_button('More Info')
+        end
       end
     end
   end
