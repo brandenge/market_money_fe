2f05b3d265754d7997998c8f5aad6833546df829
Branden Ge <86636108+brandenge@users.noreply.github.com>
2023-05-29 21:44:33 -0500

    Fix: factory bot factories to create POROs

--- spec/factories/factories.rb
+++ spec/factories/factories.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :market do
+    skip_create
+
+    id { Faker::Number.within(range: 300_000..400_000) }
     name { Faker::Company.name }
     street { Faker::Address.street_address }
     city { Faker::Address.city }
@@ -11,26 +14,24 @@ FactoryBot.define do
 
     factory :market_with_vendors do
       transient do
-        vendor_count { 5 }
-      end
-
-      after(:create) do |market, evaluator|
-        create_list(:market_vendor, evaluator.vendor_count, market: market)
+        vendors { create_list(:vendor) }
       end
     end
+
+    initialize_with { new(attributes) }
   end
 
   factory :vendor do
+    skip_create
+
+    id { Faker::Number.within(range: 11..10_000) }
     name { Faker::Commerce.vendor }
     description { "This vendor sells #{Faker::Commerce.product_name} made from #{Faker::Commerce.material} for the #{Faker::Company.industry} industry" }
     contact_name { Faker::Name.name }
     contact_phone { Faker::PhoneNumber.phone_number }
     credit_accepted { Faker::Boolean.boolean }
-  end
 
-  factory :market_vendor do
-    association :market, factory: :market, strategy: :create
-    association :vendor, factory: :vendor, strategy: :create
+    initialize_with { new(attributes) }
   end
 
   factory :automatic_teller_machine do
