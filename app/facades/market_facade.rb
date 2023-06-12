a03911a737742d3905775157b6b7efec8c5a4e9e
Branden Ge <86636108+brandenge@users.noreply.github.com>
2023-06-11 22:01:33 -0500

    Feat: add search to market facade

--- app/facades/market_facade.rb
+++ app/facades/market_facade.rb
@@ -14,9 +14,24 @@ class MarketFacade
     city = city.empty? ? nil : city
     state = state.empty? ? nil : state
     markets_data = service.markets_search(name: name, city: city, state: state)
-    markets_data.map do |market_data|
-      Market.new(market_data)
+
+    search_params = []
+    search_params.push(['name', name]) if name
+    search_params.push(['city', city]) if city
+    search_params.push(['state', state]) if state
+
+    search_results = {
+      search_params: search_params
+    }
+
+    if markets_data.is_a?(Array)
+      search_results[:markets] = markets_data.map do |market_data|
+        Market.new(market_data)
+      end
+    else
+      search_results[:errors] = markets_data[:errors][0][:detail]
     end
+    search_results
   end
 
   private
